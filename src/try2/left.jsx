90ea4a9da089f4598b562255da4457b4669ee899
Suhrid Mulay <suhridmulay@gmail.com>
2021-02-17 12:31:57 +0530

    Fixed badge sizes

--- src/try2/left.jsx
+++ src/try2/left.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './left.css';
 
+import AppStoreBadge from '../images/AppStoreBadge.png'
+import PlayStoreBadge from '../images/google-play-badge.png'
+
 function Left() {
     return (
         <React.Fragment>
@@ -11,8 +14,18 @@ function Left() {
                     <div className='left-content'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
                 </div>
                 <div className="badges">
-                    <a href="https://www.apple.com/in/app-store/"><div className='app-store-badge'></div></a>
-                    <a href="https://play.google.com/store/"><div className='play-store-badge'></div></a>
+                    <a href="https://www.apple.com/in/app-store/">
+                        {
+                            //<div className='app-store-badge'></div>
+                        }
+                        <img src={AppStoreBadge} alt="App Store"/>
+                    </a>
+                    <a href="https://play.google.com/store/">
+                        {
+                            // <div className='play-store-badge'></div>
+                        }
+                        <img src={PlayStoreBadge} alt="App Store"/>
+                    </a>
                 </div>
 
             </div>
